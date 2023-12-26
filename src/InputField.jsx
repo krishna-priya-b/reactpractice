d8892e8c058737e32d99b704c827c7d947b15ce4
Krishna Priya B <123094497+krishna-priya-b@users.noreply.github.com>
2023-12-26 17:54:48 +0400

    firts

---
+++ src/InputField.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function InputField() {
+  return (
+    <div>
+        <label htmlFor="">Name</label>
+        <input type="text" />
+    </div>
+  )
+}
+
+export default InputField
---
+++ src/LoginBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './LoginBox.css'
+import './InputField'
+import InputField from './InputField'
+
+function LoginBox() {
+  return (
+    <div className='box'>
+      <InputField/>
+      <InputField/>
+
+      <InputField/>
+
+    </div>
+  )
+}
+
+export default LoginBox
